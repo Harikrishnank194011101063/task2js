ebbe277ebb4485f7ea3f7dbfdb9bc2f6dbed0a37
Harikrishnank194011101063 <102654776+Harikrishnank194011101063@users.noreply.github.com>
2022-04-07 00:54:37 +0530

    Add files via upload

---
+++ javasc.js
@@ -0,0 +1,7 @@
+var celcius=parseInt(prompt('enter a celcius'))
+var temp=((celcius*(9/5))+32)
+console.log(celcius+ ' celcius in ferhent' +temp);
+
+var farhenheit=parseInt(prompt('enter a ferhenheit'))
+var tempreture=((farhenheit-32)*(5/9))
+console.log(farhenheit+'this is the farhenheit'+tempreture);
